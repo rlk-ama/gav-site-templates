ee7979e265f2bc4b29cacd94acf9818a3187c258
raphael <raphael.lopezkaufman@gmail.com>
2020-05-24 19:53:13 +0200

    Modularised website

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    bake: {
+      build: {  
+        files: {  
+          "index.html": "app/index.html",
+          "subscription.html": "app/subscription.html",
+          "about.html": "app/about.html",
+          "janaa.html": "app/janaa.html",
+          "outofthecube.html": "app/outofthecube.html",
+          "rene_prou.html": "app/rene_prou.html",
+          "performdoc.html": "app/performdoc.html",
+        }
+      },
+    },
+  });
+  grunt.loadNpmTasks( "grunt-bake" );
+  grunt.registerTask('default', ['bake']);
+};
